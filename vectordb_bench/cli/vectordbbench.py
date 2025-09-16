710b2c6a0f65508cf29261a090f260b0d94ec75f
zhiqiang-hhhh <seuhezhiqiang@163.com>
2025-09-16 14:17:46 +0800

    doris support
    opt ddl
    
    parallel stream load
    
    no index
    
    doris support

--- vectordb_bench/cli/vectordbbench.py
+++ vectordb_bench/cli/vectordbbench.py
@@ -20,6 +20,7 @@ from ..backend.clients.tidb.cli import TiDB
 from ..backend.clients.vespa.cli import Vespa
 from ..backend.clients.weaviate_cloud.cli import Weaviate
 from ..backend.clients.zilliz_cloud.cli import ZillizAutoIndex
+from ..backend.clients.doris.cli import Doris
 from .batch_cli import BatchCli
 from .cli import cli
 
@@ -48,6 +49,7 @@ cli.add_command(HologresHGraph)
 cli.add_command(QdrantCloud)
 cli.add_command(QdrantLocal)
 cli.add_command(BatchCli)
+cli.add_command(Doris)
 
 
 if __name__ == "__main__":
